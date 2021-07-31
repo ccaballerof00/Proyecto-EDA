69c677e4e350f1861fb6db4d355b14d383800d19
Juan Galvez <juangalvezccopa@gmail.com>
2021-07-31 01:25:01 -0500

    Setup

---
+++ cmake-build-debug/CMakeFiles/Proyecto_EDA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_EDA.dir/main.cpp.o"
+  "Proyecto_EDA"
+  "Proyecto_EDA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_EDA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
